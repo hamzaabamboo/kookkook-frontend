0485f91de66232888c9c9f14d5f30b133f391b12
pondspective <puvitza.135@gmail.com>
2020-03-14 16:21:23 +0700

    feat(): dead chicken
    -add dead chicken map

--- src/components/DeadChicken/index.js
+++ src/components/DeadChicken/index.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
+import "./index.scss";
+
+import { useTable } from "react-table";
+
+const square = isChickenDead => {
+  return <div className={`square ${isChickenDead ? "red" : ""}`}></div>;
+};
 
 const DeadChicken = () => {
+  const zoneStatus = [
+    [true, false, false, true, false, false, false, false, false, false],
+    [true, false, false, false, false, false, false, false, false, false],
+    [true, false, false, false, false, false, false, false, false, false],
+    [true, false, false, false, false, true, false, false, false, false],
+    [true, false, false, false, false, false, false, false, false, false],
+    [true, false, false, false, false, true, false, false, false, true],
+    [true, false, false, false, false, false, false, false, false, false],
+    [true, false, false, false, false, false, false, false, false, false],
+    [true, false, true, false, false, false, false, false, false, false],
+    [true, false, false, false, false, false, true, false, false, false]
+  ];
   return (
     <Container>
-      <div>DeadChicken</div>
+      <div className="text-label-ch" style={{ marginTop: "1rem" }}>
+        <p>Please select on dead chicken location</p>
+      </div>
+      <div className="map">
+        {zoneStatus.map(row => row.map(element => square(element)))}
+      </div>
     </Container>
   );
 };
