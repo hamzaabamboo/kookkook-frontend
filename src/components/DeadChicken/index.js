494acf20dceb406423dbe3f137dc18837fc8d879
pondspective <6031830321@student.chula.ac.th>
2020-03-14 17:11:23 +0700

    repush(deadchicken) :
    fix bug

--- src/components/DeadChicken/index.js
+++ src/components/DeadChicken/index.js
@@ -1,36 +1,34 @@
-import React, { useState } from "react";
-import Container from "react-bootstrap/Container";
-import "./index.scss";
-
-import { useTable } from "react-table";
+import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import './index.scss';
 
 const square = isChickenDead => {
-  return <div className={`square ${isChickenDead ? "red" : ""}`}></div>;
+    return <div className={`square ${isChickenDead ? 'red' : ''}`}></div>;
 };
 
 const DeadChicken = () => {
-  const zoneStatus = [
-    [true, false, false, true, false, false, false, false, false, false],
-    [true, false, false, false, false, false, false, false, false, false],
-    [true, false, false, false, false, false, false, false, false, false],
-    [true, false, false, false, false, true, false, false, false, false],
-    [true, false, false, false, false, false, false, false, false, false],
-    [true, false, false, false, false, true, false, false, false, true],
-    [true, false, false, false, false, false, false, false, false, false],
-    [true, false, false, false, false, false, false, false, false, false],
-    [true, false, true, false, false, false, false, false, false, false],
-    [true, false, false, false, false, false, true, false, false, false]
-  ];
-  return (
-    <Container>
-      <div className="text-label-ch" style={{ marginTop: "1rem" }}>
-        <p>Please select on dead chicken location</p>
-      </div>
-      <div className="map">
-        {zoneStatus.map(row => row.map(element => square(element)))}
-      </div>
-    </Container>
-  );
+    const zoneStatus = [
+        [true, false, false, true, false, false, false, false, false, false],
+        [true, false, false, false, false, false, false, false, false, false],
+        [true, false, false, false, false, false, false, false, false, false],
+        [true, false, false, false, false, true, false, false, false, false],
+        [true, false, false, false, false, false, false, false, false, false],
+        [true, false, false, false, false, true, false, false, false, true],
+        [true, false, false, false, false, false, false, false, false, false],
+        [true, false, false, false, false, false, false, false, false, false],
+        [true, false, true, false, false, false, false, false, false, false],
+        [true, false, false, false, false, false, true, false, false, false],
+    ];
+    return (
+        <Container>
+            <div className="text-label-ch" style={{ marginTop: '1rem' }}>
+                <p>Please select on dead chicken location</p>
+            </div>
+            <div className="map">
+                {zoneStatus.map(row => row.map(element => square(element)))}
+            </div>
+        </Container>
+    );
 };
 
 export default DeadChicken;
